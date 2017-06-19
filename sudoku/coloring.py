e7a6a08c25b105ad36de23ff14f003fc9930ce9a
Max Roecker <max.roecker@gmail.com>
2017-06-19 18:57:22 -0300

    Add coloring module

---
+++ sudoku/coloring.py
@@ -0,0 +1,39 @@
+"""Graph coloring algorithms."""
+
+import networkx as nx
+
+
+def welsh_powell(graph):
+    """Runs the Welsh-Powell algorithm to color a graph.
+
+    Arguments:
+        graph (networkx.Graph): a graph.
+    """
+    raise NotImplementedError('')
+
+
+def sequencial_coloring(graph):
+    """Runs the Sequencial Coloring algorithm to color a graph.
+
+    Arguments:
+        graph (networkx.Graph): a graph.
+    """
+    raise NotImplementedError('')
+
+
+def class_coloring(graph):
+    """Runs the Class Coloring algorithm to color a graph.
+
+    Arguments:
+        graph (networkx.Graph): a graph.
+    """
+    raise NotImplementedError('')
+
+
+def dsatur(graph):
+    """Runs the Degree of Saturation heuristic algorithm to color a graph.
+
+    Arguments:
+        graph (networkx.Graph): a graph.
+    """
+    raise NotImplementedError('')
